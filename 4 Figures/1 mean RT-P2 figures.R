440dd7dc159dd56dc15c1ba116064540d64afad6
hiv8r3 <hiv8r3@mail.missouri.edu>
2019-09-13 18:23:06 -0500

    RT->P2 by condition figures

--- 4 Figures/1 mean RT-P2 figures.R
+++ 4 Figures/1 mean RT-P2 figures.R
@@ -11,8 +11,8 @@ theme_hve = function (base_size = 20,
              base_rect_size = base_rect_size) %+replace% 
     theme(panel.background = element_rect(fill = "white", colour = NA), 
           panel.border = element_rect(fill = NA, colour = "grey20"), 
-          panel.grid = element_line(colour = "white"), 
-          panel.grid.minor = element_line(size = rel(0.5)), 
+          panel.grid = element_blank(), 
+          panel.grid.minor = element_blank(), 
           strip.background = element_rect(fill = "grey85", colour = "grey20"), 
           legend.key = element_rect(fill = "white", colour = NA), 
           plot.title = element_text(hjust = 0.5),
@@ -35,42 +35,13 @@ ggplot(ERPfix, aes(Race, RT, fill = Fix)) +
   scale_fill_manual(values=c("white","grey70"), guide = guide_legend(title = "Fixation")) +
   theme_hve() +
   theme(legend.position = "bottom") +
-  coord_cartesian(ylim = c(400, 500))
-
-dev.off()
-
-win.metafile("./4 Figures/Study1_RT_point.wmf")
-
-ggplot(ERPfix, aes(Race, RT, shape = Fix), color = "black") +
-  stat_summary(fun.y = mean, geom = "point", size=4, position = position_dodge(width=.9)) +
-  stat_summary(fun.data = mean_cl_normal, geom = "errorbar", position = position_dodge(width=.9), width = .2, size = 1) + 
-  labs(y = "Reaction Time", x = "Target Race") +
-  scale_shape_manual(values=c(1, 16)) +
-  #scale_fill_manual(values=c("white","grey70"), guide = guide_legend(title = "Fixation")) +
-  theme_hve() +
-  theme(legend.position = "bottom") +
-  coord_cartesian(ylim = c(440, 475))
+  coord_cartesian(ylim = c(425, 525))
 
 dev.off()
 
 
 # P2
-win.metafile("./4 Figures/Study1_P2_v1.wmf")
-
-ggplot(ERPfix, aes(Race, MeanAmp, group = Fix)) +
-  stat_summary(fun.y = mean, geom = "point", position = position_dodge(width=.9), color = "blue", size = 3) +
-  stat_summary(fun.data = mean_cl_normal, geom = "errorbar", position = position_dodge(width=.9), width = .2, size = 2, color = "blue") +
-  labs(y = "P2 amplitude", x = "Target Race") +
-  #scale_fill_manual(guide = guide_legend(title = "Fixation")) +
-  theme_hve() +
-  coord_cartesian(ylim = c(1, 5)) +
-  scale_y_continuous(position = "right") +
-  theme(axis.title = element_text(color = "blue"),
-        axis.text = element_text(color = "blue"))
-
-dev.off()
-
-win.metafile("./4 Figures/Study1_P2_point_v2.wmf")
+win.metafile("./4 Figures/Study1_P2_point.wmf")
 
 ggplot(ERPfix, aes(Race, MeanAmp, shape = Fix)) +
   stat_summary(fun.y = mean, geom = "point", size=4, position = position_dodge(width=.9), color = "blue") +
@@ -94,49 +65,75 @@ RaceGen = read.delim("./2 Race-Gen/AllSubs_acceptedTrials_long_nobe_nobs_withRT_
 RG_racetask = filter(RaceGen, Task == "Race")
 RG_gentask = filter(RaceGen, Task == "Gender")
 
-# Race Task
+facet_labels = c(eyes = "Eyes fixation", forehead = "Forehead fixation")
 
-ggplot(RG_racetask, aes(TarRace, RT, fill = Fix)) +
-  facet_wrap(~TarGender) +
+#RT
+
+# Race Task
+win.metafile("./4 Figures/Study2_RT_RaceTask_bar.wmf")
+
+ggplot(RG_racetask, aes(TarRace, RT, fill = TarGender)) +
+  facet_wrap(~Fix, labeller=labeller(Fix = facet_labels)) +
   stat_summary(fun.y = mean, geom = "bar", position = "dodge") +
   stat_summary(fun.data = mean_cl_normal, geom = "errorbar", position = position_dodge(width=.9), width = .2) +
   labs(y = "Reaction Time", x = "Target Race") +
   scale_fill_manual(values=c("black","grey70"), guide = guide_legend(title = "Fixation")) +
   theme_hve() +
-  coord_cartesian(ylim = c(400, 500)) +
-  ggtitle("Race task")
+  coord_cartesian(ylim = c(410, 510)) +
+  theme(legend.position = "bottom") 
 
-ggplot(RG_racetask, aes(TarRace, value, fill = Fix)) +
-  facet_wrap(~TarGender) +
-  stat_summary(fun.y = mean, geom = "bar", position = "dodge") +
-  stat_summary(fun.data = mean_cl_normal, geom = "errorbar", position = position_dodge(width=.9), width = .2) +
-  labs(y = "P2 amplitude", x = "Target Race") +
-  scale_fill_manual(values=c("black","grey70"), guide = guide_legend(title = "Fixation")) +
-  theme_hve() +
-#  coord_cartesian(ylim = c(400, 500)) +
-  ggtitle("Race task")
+dev.off()
 
 # Gen Task
+win.metafile("./4 Figures/Study2_RT_GenTask_bar.wmf")
 
-ggplot(RG_gentask, aes(TarRace, RT, fill = Fix)) +
-  facet_wrap(~TarGender) +
+ggplot(RG_gentask, aes(TarRace, RT, fill = TarGender)) +
+  facet_wrap(~Fix, labeller=labeller(Fix = facet_labels)) +
   stat_summary(fun.y = mean, geom = "bar", position = "dodge") +
   stat_summary(fun.data = mean_cl_normal, geom = "errorbar", position = position_dodge(width=.9), width = .2) +
   labs(y = "Reaction Time", x = "Target Race") +
   scale_fill_manual(values=c("black","grey70"), guide = guide_legend(title = "Fixation")) +
   theme_hve() +
-  coord_cartesian(ylim = c(400, 500)) +
-  ggtitle("Race task")
+  coord_cartesian(ylim = c(410, 510)) +
+  theme(legend.position = "bottom") 
 
-ggplot(RG_gentask, aes(TarRace, value, fill = Fix)) +
-  facet_wrap(~TarGender) +
-  stat_summary(fun.y = mean, geom = "bar", position = "dodge") +
-  stat_summary(fun.data = mean_cl_normal, geom = "errorbar", position = position_dodge(width=.9), width = .2) +
-  labs(y = "P2 amplitude", x = "Target Race") +
-  scale_fill_manual(values=c("black","grey70"), guide = guide_legend(title = "Fixation")) +
+dev.off()
+
+# P2
+
+# Race Task
+win.metafile("./4 Figures/Study2_P2_RaceTask_point.wmf")
+
+ggplot(RG_racetask, aes(TarRace, value, fill = TarGender, shape = TarGender)) +
+  facet_wrap(~Fix, labeller=labeller(Fix = facet_labels)) +
+  stat_summary(fun.y = mean, geom = "point", size=4, position = position_dodge(width=.9), color = "blue") +
+  stat_summary(fun.data = mean_cl_normal, geom = "errorbar", position = position_dodge(width=.9), width = .2, size = 1, color = "blue") + 
+  scale_shape_manual(values=c(1, 16)) +
   theme_hve() +
-  #  coord_cartesian(ylim = c(400, 500)) +
-  ggtitle("Race task")
+  theme(legend.position = "bottom") +
+  scale_y_continuous(position = "right") +
+  coord_cartesian(ylim = c(1, 5)) +
+  theme(axis.title = element_text(color = "blue"),
+        axis.text = element_text(color = "blue"))
+
+dev.off()
+
+# Gender Task
+win.metafile("./4 Figures/Study2_P2_GenderTask_point.wmf")
+
+ggplot(RG_gentask, aes(TarRace, value, fill = TarGender, shape = TarGender)) +
+  facet_wrap(~Fix, labeller=labeller(Fix = facet_labels)) +
+  stat_summary(fun.y = mean, geom = "point", size=4, position = position_dodge(width=.9), color = "blue") +
+  stat_summary(fun.data = mean_cl_normal, geom = "errorbar", position = position_dodge(width=.9), width = .2, size = 1, color = "blue") + 
+  scale_shape_manual(values=c(1, 16)) +
+  theme_hve() +
+  theme(legend.position = "bottom") +
+  scale_y_continuous(position = "right") +
+  coord_cartesian(ylim = c(1, 5)) +
+  theme(axis.title = element_text(color = "blue"),
+        axis.text = element_text(color = "blue"))
+
+dev.off()
 
 # Study 3 -----------------------------------------------------------------
 
